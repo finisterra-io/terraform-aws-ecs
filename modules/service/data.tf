1a29d4de67e2761a221efc66a18f576731e356b7
xneyder <xneyder@gmail.com>
2023-08-06 22:04:36 -0400

    Add files

--- modules/service/data.tf
+++ modules/service/data.tf
@@ -1,4 +1,10 @@
+data "aws_service_discovery_private_dns_namespace" "selected" {
+  count = length(lookup(var.service_registries, "namespace_name", "")) > 0 ? 1 : 0
+  name  = lookup(var.service_registries, "namespace_name", null)
+}
+
 data "aws_service_discovery_service" "selected" {
-  count = length(lookup(var.service_registries, "registry_name", "")) > 0 ? 1 : 0
-  name  = lookup(var.service_registries, "registry_name", null)
+  count        = length(lookup(var.service_registries, "registry_name", "")) > 0 ? 1 : 0
+  name         = lookup(var.service_registries, "registry_name", null)
+  namespace_id = data.aws_service_discovery_private_dns_namespace.selected[0].id
 }
