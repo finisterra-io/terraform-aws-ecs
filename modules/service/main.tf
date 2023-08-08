9c7e985babcc15407afffa2f2df0899087e039a3
xneyder <xneyder@gmail.com>
2023-08-07 23:41:26 -0400

    Add files

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -450,7 +450,7 @@ resource "aws_lb_listener" "this" {
 resource "aws_lb_listener_rule" "this" {
   count = var.create_aws_lb_listener_rule ? 1 : 0
 
-  listener_arn = var.create_aws_lb_listener ? aws_lb_listener.this[0].arn : data.aws_lb_listener.default[0].arn
+  listener_arn = var.create_aws_lb_listener ? aws_lb_listener.this[0].arn : data.aws_lb_listener.listener_rule[0].arn
   priority     = var.listener_rule_priority
 
   action {
