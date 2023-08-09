2aedc9637865cc49b8b4b3ae300967bae2262b0a
xneyder <xneyder@gmail.com>
2023-08-08 23:06:24 -0400

    Add files

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -467,7 +467,7 @@ resource "aws_lb_listener_rule" "this" {
 
       # HTTP Header condition
       dynamic "host_header" {
-        for_each = condition.value.host_header
+        for_each = [condition.value[0].host_header[0]]
         content {
           values = host_header.value.values
         }
