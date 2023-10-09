43706c9c7ec6a80c74f58004ab8f5b1dde966694
xneyder <xneyder@gmail.com>
2023-10-09 15:41:18 -0400

    Add subnet names

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -1362,7 +1362,7 @@ locals {
 
 data "aws_subnet" "default" {
   count  = local.create_security_group ? length(var.subnet_names) : 0
-  vpc_id = var.vpc_name != "" ? data.aws_vpc.default[0].id : var.vpc_id
+  vpc_id = data.aws_vpc.default[0].id
   filter {
     name   = "tag:Name"
     values = [var.subnet_names[count.index]]
@@ -1375,7 +1375,7 @@ resource "aws_security_group" "this" {
   name        = var.security_group_use_name_prefix ? null : local.security_group_name
   name_prefix = var.security_group_use_name_prefix ? "${local.security_group_name}-" : null
   description = var.security_group_description
-  vpc_id      = data.aws_subnet.default[0].vpc_id
+  vpc_id      = data.aws_vpc.default[0].id
 
   tags = merge(var.tags, var.security_group_tags)
 
