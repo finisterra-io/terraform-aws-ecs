cffd4863813a15dc696c70787b302d0014adc22b
xneyder <xneyder@gmail.com>
2023-11-07 21:05:22 -0500

    Fixes

--- modules/service/variables.tf
+++ modules/service/variables.tf
@@ -864,11 +864,7 @@ variable "autoscaling_tags" {
 
 variable "network_configuration" {
   description = "Network configuration for the service"
-  type = map(object({
-    assign_public_ip = bool
-    security_groups  = list(string)
-    subnets          = list(string)
-  }))
-  default = {}
+  type        = any
+  default     = {}
 }
 
